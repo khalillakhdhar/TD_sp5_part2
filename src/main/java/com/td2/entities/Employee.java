a41325a6067260f9b2fec5eba3fc0e28ee729b5f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 14:22:38 +0100

    Create Employee.java

---
+++ src/main/java/com/td2/entities/Employee.java
@@ -0,0 +1,115 @@
+package com.td2.entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "prenom")
+    private String prenom;
+    @Basic(optional = false)
+    @Column(name = "nom")
+    private String nom;
+    @Basic(optional = false)
+    @Column(name = "telephone")
+    private String telephone;
+    @Basic(optional = false)
+    @Column(name = "adresse")
+    private String adresse;
+    @Basic(optional = false)
+    @Column(name = "grade")
+    private String grade;
+    @Basic(optional = false)
+    @Column(name = "age")
+    private int age;
+    public Employee() {
+    }
+
+    public Employee(Integer id) {
+        this.id = id;
+    }
+
+    public Employee(Integer id, String email, String prenom, String nom, String telephone, String adresse, String grade) {
+        this.id = id;
+        this.email = email;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.grade = grade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+
+    
+}
