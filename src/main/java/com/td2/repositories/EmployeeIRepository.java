152be429d6e14dd19e4eaa45d3d6df2445c0ba21
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 14:25:26 +0100

    Update EmployeeIRepository.java

--- src/main/java/com/td2/repositories/EmployeeIRepository.java
+++ src/main/java/com/td2/repositories/EmployeeIRepository.java
@@ -2,6 +2,8 @@ package com.td2.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.td2.entities.Employee;
+
 public interface EmployeeIRepository extends CrudRepository<Employee,Long> {
 
 }
