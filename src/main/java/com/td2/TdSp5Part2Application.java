f9f36cbaa5c2af2c1e415d2ba92b5e114fd82bf9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 14:16:24 +0100

    Initial commit

---
+++ src/main/java/com/td2/TdSp5Part2Application.java
@@ -0,0 +1,13 @@
+package com.td2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSp5Part2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdSp5Part2Application.class, args);
+	}
+
+}
